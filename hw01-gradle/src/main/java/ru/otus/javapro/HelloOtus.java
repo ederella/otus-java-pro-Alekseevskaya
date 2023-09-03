3cc1d49fc119e0fa10ebf6d90a27c120068ab778
Veronica Alexeevskaya <ederella@yandex.ru>
2023-09-03 13:41:57 +0300

    Homework #1

---
+++ hw01-gradle/src/main/java/ru/otus/javapro/HelloOtus.java
@@ -0,0 +1,9 @@
+package ru.otus.javapro;
+import com.google.common.base.Strings;
+
+public class HelloOtus {
+
+    public static void main(String[] args) {
+        System.out.println(Strings.repeat("Pizza!\n", 10));
+    }
+}
