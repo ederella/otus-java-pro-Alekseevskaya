49e990266a4838385150e9f647f03d04feca1f34
Veronika <ederella@ya.ru>
2023-09-24 23:25:59 +0300

    homework annotations + reporting messages

--- hw05-annotations/src/main/java/ru/otus/javapro/annotations/TestRunner.java
+++ hw05-annotations/src/main/java/ru/otus/javapro/annotations/TestRunner.java
@@ -5,11 +5,12 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 
 public class TestRunner {
-    ArrayList <Method> methodsBefore;
-    ArrayList <Method> methodsTest;
-    ArrayList<Method> methodsAfter;
+    private ArrayList <Method> methodsBefore;
+    private ArrayList <Method> methodsTest;
+    private ArrayList<Method> methodsAfter;
 
     public TestRunner(Class<?> cls) throws Exception {
+
         methodsBefore = new ArrayList<>();
         methodsTest = new ArrayList<>();
         methodsAfter = new ArrayList<>();
@@ -19,31 +20,34 @@ public class TestRunner {
         for (Method method : methods) {
 
             int methodAnnotationCount = 0;
-            if(method.getAnnotation(Test.class) !=null){
+            if (method.getAnnotation(Test.class) != null) {
                 methodsTest.add(method);
                 methodAnnotationCount++;
             }
-            if(method.getAnnotation(Before.class) !=null){
+            if (method.getAnnotation(Before.class) != null) {
                 methodsBefore.add(method);
                 methodAnnotationCount++;
             }
-            if(method.getAnnotation(After.class) !=null){
+            if (method.getAnnotation(After.class) != null) {
                 methodsAfter.add(method);
                 methodAnnotationCount++;
             }
-            if(methodAnnotationCount > 1)
+            if (methodAnnotationCount > 1) {
                 throw new Exception("Annotations should not be repeated");
+            }
         }
-
     }
     public void run() throws Exception {
+        int successCount = 0;
         for(Method method: methodsTest){
             method.setAccessible(true);
             Object obj = method.getDeclaringClass().getDeclaredConstructor().newInstance();
 
             try {
                 runAllBefore(obj);
+                System.out.println("call " + getStringName(method));
                 method.invoke(obj);
+                successCount++;
 
             } catch (Throwable e) {
                 e.printStackTrace();
@@ -51,11 +55,20 @@ public class TestRunner {
 
             runAllAfter(obj);
         }
+        System.out.println("---------REPORT--------");
+        System.out.println("TOTAL: "+ methodsTest.size());
+        System.out.println("SUCCESS: "+ successCount);
+        System.out.println("----------------------");
+    }
+
+    private static String getStringName(Method method) {
+        return method.getDeclaringClass().getName() + '.' + method.getName();
     }
 
     private void runAllAfter(Object obj) throws IllegalAccessException, InvocationTargetException {
         for (Method methodAfter: methodsAfter) {
             methodAfter.setAccessible(true);
+            System.out.println("call "+getStringName(methodAfter));
             methodAfter.invoke(obj);
         }
     }
@@ -63,8 +76,8 @@ public class TestRunner {
     private void runAllBefore(Object obj) throws IllegalAccessException, InvocationTargetException {
         for (Method methodBefore: methodsBefore) {
             methodBefore.setAccessible(true);
+            System.out.println("call "+getStringName(methodBefore));
             methodBefore.invoke(obj);
         }
     }
-
 }
--- hw05-annotations/src/main/java/ru/otus/javapro/testclasses/AnotherTestCLass.java
+++ hw05-annotations/src/main/java/ru/otus/javapro/testclasses/AnotherTestCLass.java
@@ -15,7 +15,6 @@ public class AnotherTestCLass {
 
     @Before
     void beforePrint() {
-
         System.out.println("Start another");
     }
 
