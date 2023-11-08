253e65889e5510b6b6d873e284fb68ff0c38a20a
Veronika <ederella@ya.ru>
2023-11-08 21:21:38 +0300

    homework

--- hw07-patterns/src/main/java/ru/otus/processor/homework/ProcessorSwapFields.java
+++ hw07-patterns/src/main/java/ru/otus/processor/homework/ProcessorSwapFields.java
@@ -1,19 +1,11 @@
 package ru.otus.processor.homework;
 
 import ru.otus.model.Message;
-import ru.otus.model.ObjectForMessage;
 import ru.otus.processor.Processor;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class ProcessorSwapFields implements Processor {
     @Override
     public Message process(Message message) {
-        ObjectForMessage field11 = new ObjectForMessage();
-        field11.setData(List.of(message.getField11()));
-        String field13 = message.getField13() != null ? message.getField13().getData().stream().collect(Collectors.joining(", ")) : null;
-
-        return message.toBuilder().field11(field13).field13(field11).build();
+        return message.toBuilder().field11(message.getField12()).field12(message.getField11()).build();
     }
 }
